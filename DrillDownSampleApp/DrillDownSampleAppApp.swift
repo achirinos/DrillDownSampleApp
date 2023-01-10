db6179f9322c9e3c4974df4f267629b19cf5ef09
asdrubalchirinos <achirinos@gmail.com>
2023-01-10 10:42:34 -0600

    Initial Commit

---
+++ DrillDownSampleApp/DrillDownSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrillDownSampleAppApp.swift
+//  DrillDownSampleApp
+//
+//  Created by Asdrubal Chirinos on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DrillDownSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
